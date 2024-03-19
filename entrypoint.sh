7e03c8609d7a5d2104fc1cdf4714cbdc2c515b04
ricksonoliveira <riksongleison471@gmail.com>
2024-03-19 19:48:30 -0300

    create postgres app if not exists anyway

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,13 +49,14 @@ if [ "$EVENT_TYPE" = "closed" ]; then
   exit 0
 fi
 
+# Create the Fly PostgreSQL instance if it doesn't exist.
+if ! flyctl status --app "$postgres_app"; then
+    flyctl postgres create --name "$postgres_app" --region "$region"
+fi
+
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
   flyctl apps create --name "$app" --org "$org"
-
-  if ! flyctl status --app "$postgres_app"; then
-    flyctl postgres create --name "$postgres_app" --region "$region"
-  fi
 
   flyctl postgres attach "$postgres_app" --app "$app"
   flyctl deploy $detach --app "$app" --region "$region" --strategy immediate --remote-only
